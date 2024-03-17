791fbf19c1238a1c639abd4f8419ff63913503e5
vitouaj <vitouaj68@gmail.com>
2024-03-17 21:19:53 +0700

    test basic html, css and js for navigationbar

--- src/frontend/ems-vue/src/router/index.ts
+++ src/frontend/ems-vue/src/router/index.ts
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: 
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/LandingPageView.vue')
+    }
     // {
     //   path: '/about',
     //   name: 'about',
