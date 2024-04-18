01f170f3c5d771cdc58e35791b110f5c81aeb1b9
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-04-18 17:38:04 +0700

    Feat: Adding User & Admin Mode

---
+++ src/frontend/ems-vue/src/AuthForm/Admin/AuthAdminScreen.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>Im admin.</h1>
+    <form v-if="mode === 'login'" @submit.prevent="login">
+      <input type="email" v-model="loginData.email" placeholder="Email" required>
+      <input type="password" v-model="loginData.password" placeholder="Password" required>
+      <button type="submit">Log In</button>
+    </form>
+    
+
+    <form v-else @submit.prevent="signup">
+      <input type="text" v-model="signupData.name" placeholder="Name" required>
+      <input type="email" v-model="signupData.email" placeholder="Email" required>
+      <input type="password" v-model="signupData.password" placeholder="Password" required>
+      <button type="submit">Sign Up</button>
+    </form>
+    <p>
+      <button @click="toggleMode">{{ mode === 'login' ? 'Sign Up' : 'Log In' }}</button>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const mode = ref('login'); // Default mode is login
+const loginData = ref({
+  email: '',
+  password: ''
+});
+const signupData = ref({
+  name: '',
+  email: '',
+  password: ''
+});
+
+const login = () => {
+};
+
+const signup = () => {
+};
+
+const toggleMode = () => {
+
+};
+</script>
+
+<style scoped>
+/* Your component styles */
+</style>
