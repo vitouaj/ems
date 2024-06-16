97f4afe3c8284ca61d48de8f3f8bc9849f4c4ce7
Chanjnint Lim <100104333+Chanjnint21@users.noreply.github.com>
2024-06-16 10:04:58 +0700

    Update EventList.vue

--- src/frontend/ems-vue/src/views/AdminSite/components/EventList.vue
+++ src/frontend/ems-vue/src/views/AdminSite/components/EventList.vue
@@ -1,29 +1,26 @@
 <template>
-    <v-row>
-        <template>
-           <v-col cols="12" class="d-flex justify-space-around">
-                <div>
-                    <p>Event Name</p>
-                </div>
-                <div>
-                    <p>Ongoing</p>
-                </div>
-                <div>
-                    <p> {{ countDay }} / {{ dayOfEvent }}</p>
-                </div>
-            </v-col> 
-        </template>   
-    </v-row>
+    <v-col cols="12" class="d-flex justify-space-around pb-0">
+        <v-col cols="6" class="py-0">
+            <p>{{ event.name }}</p>
+        </v-col>
+        <v-col cols="3" class="py-0 d-flex justify-start">
+            <p v-if="event.status" style="color: rgb(255, 127, 0)">ongoing</p>
+            <p v-else style="color: #004B8D">ended</p>
+        </v-col>
+        <v-col cols="3" class="d-flex justify-end py-0">
+            <p> {{ event.participators }} / {{ event.t_participators }}</p>
+        </v-col>
+    </v-col> 
     <!-- <p>hey</p> -->
 </template>
 
 <script>
 export  default {
-    data() {
-        return {
-            countDay: 5,
-            dayOfEvent: 100
-        }
-    }
+    props: {
+      event: {
+        type: Object,
+        required: true
+      }
+    },
 }
 </script>
