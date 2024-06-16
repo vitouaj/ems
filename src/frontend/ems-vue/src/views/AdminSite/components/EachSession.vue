60c4719d48acedd2e62b356a0ad0a997e712cc0b
Chanjnint Lim <100104333+Chanjnint21@users.noreply.github.com>
2024-06-16 10:04:18 +0700

    Update EachSession.vue

--- src/frontend/ems-vue/src/views/AdminSite/components/EachSession.vue
+++ src/frontend/ems-vue/src/views/AdminSite/components/EachSession.vue
@@ -1,58 +1,81 @@
 <template>
     <v-row>
-        <v-col cols="12" class="d-flex justify-center pb-0">
-            <span class="text-caption" style="color:#FF7F00">session 1</span>
-        </v-col>
-        <v-col cols="12" class="py-0">
-            <label for="eventName" class="labelText">Title</label><br />
-            <text-field
-                placeholder="e.g name of session"
-            ></text-field>
-        </v-col>
-        <v-col cols="6" class="py-0">
-            <label for="eventName" class="labelText">Date</label><br />
-            <text-field
-                placeholder="Date of session"
-            ></text-field>
-        </v-col>
-        <v-col cols="6" class="py-0">
-            <label for="StartDate" class="labelText">Start Time</label><br />
-            <text-field
-                placeholder="HH/MM/SS"
-            ></text-field>
-        </v-col>
-        <v-col cols="6"></v-col>
-        <v-col cols="6" class="py-0">
-            <label for="EndDate" class="labelText">End Time</label><br />
-            <text-field
-                placeholder="HH/MM/SS"
-            ></text-field>
-        </v-col>
-        <v-col cols="12" class="py-0">
-            <label for="EndDate" class="labelText">Description</label><br />
-            <v-textarea
-                variant="solo"
-                row-height="20"
-                rows="2"
-                auto-grow
-                placeholder="type your description here..."
-            ></v-textarea>
-        </v-col>
+      <v-col cols="12" class="d-flex justify-center pb-0">
+        <span class="text-caption" style="color:#FF7F00">{{ session.title }}</span>
+      </v-col>
+      <v-col cols="12" class="py-0">
+        <label for="eventName" class="labelText">Title</label><br />
+        <v-text-field
+          v-model="localSession.title"
+          placeholder="e.g name of session"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6" class="py-0">
+        <label for="eventName" class="labelText">Date</label><br />
+        <v-text-field
+          v-model="localSession.date"
+          placeholder="Date of session"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6" class="py-0">
+        <label for="StartDate" class="labelText">Start Time</label><br />
+        <v-text-field
+          v-model="localSession.start_time"
+          placeholder="HH/MM/SS"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="6"></v-col>
+      <v-col cols="6" class="py-0">
+        <label for="EndDate" class="labelText">End Time</label><br />
+        <v-text-field
+          v-model="localSession.end_time"
+          placeholder="HH/MM/SS"
+        ></v-text-field>
+      </v-col>
+      <v-col cols="12" class="py-0">
+        <label for="EndDate" class="labelText">Description</label><br />
+        <v-textarea
+          v-model="localSession.description"
+          variant="solo"
+          row-height="20"
+          rows="2"
+          auto-grow
+          placeholder="type your description here..."
+        ></v-textarea>
+      </v-col>
     </v-row>
-</template>
-
-<script>
-import TextField from './TextField.vue';
-
-export default {
+  </template>
+  
+  <script>
+  import TextField from './TextField.vue';
+  
+  export default {
     components: {
-        TextField,
+      TextField,
+    },
+    props: {
+      session: Object,
+      index: Number
+    },
+    data() {
+      return {
+        localSession: { ...this.session }
+      };
+    },
+    watch: {
+      localSession: {
+        handler(newSession) {
+          this.$emit('update-session', newSession);
+        },
+        deep: true
       }
-}
-</script>
-
-<style>
-.labelText{
+    }
+  };
+  </script>
+  
+  <style>
+  .labelText {
     color: #004B8D;
-}
-</style>
+  }
+  </style>
+  
